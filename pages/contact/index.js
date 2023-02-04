5b5e81c9d8f95ddf17d3b32570c6133b7392a362
Origin <git@github.com:Koumo12/Bootstrap-Portefolio-CV.git>
2023-02-04 08:44:57 +0100

    Tarif Page and Contact page modificated

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -7,7 +7,7 @@ const  Contact = () => {
  
   const [state, handleSubmit] = useForm("xlekrakp");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+      return (<p>Thanks for joining!</p>);
   }
   return (
       <div className="container p-5 contact">
@@ -16,61 +16,68 @@ const  Contact = () => {
                   <h1><strong>Contact</strong></h1>
               </div>
           </div>
-          <div className="row " >
-              <div className="col-md-3"></div>
-              <div className="col-md-6 border bordure" >
-                  <form onSubmit={handleSubmit}>
-                      <div className="row mt-5">
-                          <div className="col-lg-2 pb-5">
-                              <label  htmlFor="email">
-                                  Email:
-                              </label>
-                          </div>
-                          <div className="col-lg-10">
-                              <input className="form-control"
-                                  id="email"
-                                  type="email" 
-                                  name="email"
-                              />
-                              <ValidationError 
-                                  prefix="Email" 
-                                  field="email"
-                                  errors={state.errors}
-                              />
-                          </div>
-                      </div>
-                      <div className="row pb-5">
-                          <div className="col-lg-2 pb-5">
-                              <label  htmlFor="email">
-                                  Message:
-                              </label>
-                          </div>
-                          <div className="col-lg-10">
-                              <textarea
-                                  className="form-control"
-                                  id="message"
-                                  name="message"
-                              />
-                              <ValidationError 
-                                  prefix="Message" 
-                                  field="message"
-                                  errors={state.errors}
-                              />
-                          </div>
-                      </div>
-                      <div className="row mb-5">
-                          <div className="submit">
-                              <button type="submit" className="button outline" disabled={state.submitting}>
-                                  Envoyer
-                              </button>
-                          </div>
-                      </div>
-                  </form>
-              </div>
-              <div className="col-md-3"></div>
-          </div>            
-      </div>
-  )
+          <div className="row border bordure" >
+                <div className="col-md-6 mt-5 ">
+                    <p><strong>Nous sommes à votre disposition pour toutes vos demandes et besoins. N´hésitez pas à nous contacter pour plus information ou pour vos rendez-vous:</strong></p>
+                    <div className="mt-3"><h3><strong className="farb">Charles Cantin</strong></h3></div>
+                    <div className=""><strong className="farb">Adresse:</strong> Rue Dzébé 13</div>
+                    <span className=""><strong className="farb">BP:</strong> 43092</span><span className="mt-3"><strong className="farb">     Ville:</strong> Adopkè</span>
+                    <div className=""><strong className="farb">Tel:</strong> 00336 254 174 12 30</div>
+                    <div className=""><strong className="farb">Mobil:</strong> 00336 152 174 12 30</div>
+                    <div className="mb-5"><strong className="farb">Email:</strong> charles.cantin@gmail.com</div>
+                </div>
+                <div className="col-md-6 border bordurGauche" >
+                    <form onSubmit={handleSubmit}>
+                        <div className="row mt-5">
+                            <div className="col-lg-2 col-md-2 col-sm-2 pb-5">
+                                <label className="farb" htmlFor="email">
+                                    <strong>Email:</strong>
+                                </label>
+                            </div>
+                            <div className="col-lg-10 col-md-10 col-sm-10">
+                                <input className="form-control"
+                                    id="email"
+                                    type="email" 
+                                    name="email"
+                                />
+                                <ValidationError 
+                                    prefix="Email" 
+                                    field="email"
+                                    errors={state.errors}
+                                />
+                            </div>
+                        </div>
+                        <div className="row pb-5">
+                            <div className="col-lg-2 col-md-3 col-sm-2 pb-5">
+                                <label className="farb" htmlFor="email">
+                                    <strong>Message:</strong>
+                                </label>
+                            </div>
+                            <div className="col-lg-10 col-md-9 col-sm-10">
+                                <textarea
+                                    className="form-control"
+                                    id="message"
+                                    name="message"
+                                />
+                                <ValidationError 
+                                    prefix="Message" 
+                                    field="message"
+                                    errors={state.errors}
+                                />
+                            </div>
+                        </div>
+                        <div className="row mb-5">
+                            <div className="submit">
+                                <button type="submit" className="button outline" disabled={state.submitting}>
+                                    Envoyer
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>              
+            </div>            
+        </div>
+    )
 }
 
 export default Contact;
--- pages/tarifs&prestations/index.js
+++ pages/tarifs&prestations/index.js
@@ -1,28 +1,52 @@
 
 import React from 'react';
+import Link from 'next/link';
 
 const TarifsPrestation = ({data}) => {
     return (
         <div className="">           
             <div className="container p-5">
                 <div className="row">
-                    <div className="col-md-12 text-center mb-3"><h1><strong>Nos offres</strong></h1></div>
+                    <div className="col-md-12 text-center "><h1><strong>Nos offres</strong></h1></div>
                 </div>
                 
-            
-                {data.map((ev) =>                 
-                    <>  
-                        {
-                            <div className="row p-4 border bordure" key={ev.id}>
-                                <div className="col-md-2 mb-4 " ></div>
-                                <div className="col-md-3 mb-4 " ><strong>{ev.title}</strong></div>
-                                <div className="col-md-2 mb-4 " ><strong>{ev.prix}</strong></div>
-                                <div className="col-md-5 mb-4 ">{ev.description}</div>
-                            </div>
-                        }
-                    </>
-                )}
-        
+                <div className='row'>
+                    {data.map((ev) =>                 
+                        <>  
+                            {
+                                <div className='col-lg-4 col-md-6 col-sm-12'>
+                                    <div className="card mt-4 text-center ">   
+                                        <div className='card-body'>
+                                            <div className="card-title ">
+                                                    <strong className='header d-block'>
+                                                        {ev.title}
+                                                    </strong>
+                                                </div>                                     
+                                                <ul className="list-group list-group-flush">                                           
+                                                    <li className="list-group-item">
+                                                        <strong> 
+                                                            {ev.prix} 
+                                                        </strong>
+                                                    </li>
+                                                    <li className="list-group-item hauteur">
+                                                        {ev.description}
+                                                    </li>
+                                                    <li className="list-group-item pt-4">
+                                                        <span type="button"  >
+                                                            <Link className='button outline' href={'/contact'}>
+                                                                Rendez-vous
+                                                            </Link>  
+                                                        </span>
+                                                    </li>
+                                                </ul>
+                                        </div>
+                                    </div>
+                                </div>                           
+                            }
+                        </>
+                    )}
+                </div>
+                
             </div>
         </div>
     );
@@ -31,12 +55,13 @@ const TarifsPrestation = ({data}) => {
 
 export default TarifsPrestation;
 
-export async function getServerSideProps() {
 
+export async function getServerSideProps () {
     const {tarifsPrestation} = await import('/data/data.json');
+
     return {
         props: {
-          data: tarifsPrestation
+          data: tarifsPrestation,
         }
     }
 }
